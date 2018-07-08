bb5d5025436e8236099372ce4f967447ecfa790f
x3tsunayh <tyhshana@hotmail.com>
2018-07-08 15:03:37 +0800

    submission of hackathon unity files

---
+++ A Good Idea/Assets/ButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+    public Button button;
+    public float incrVal = 0.07f;
+    public float decrVal = 0.03f;
+    private float currVal = 0.5f;
+
+    // Update is called once per frame
+    void Update() {
+        decrement();
+        updateColor();
+
+    }
+
+    void updateColor()
+    {
+        ColorBlock cb = button.colors;
+        cb.normalColor = new Color(1 - currVal, currVal, currVal);
+        button.colors = cb;
+    }
+    
+        void increment()
+    {
+        if (currVal + incrVal <= 1)
+        {
+            currVal += incrVal;
+        }
+    }
+    void decrement()
+    {
+        float valToDecr = decrVal * Time.deltaTime;
+
+        Debug.Log(currVal);
+
+        if (currVal - valToDecr >= 0)
+        {
+            currVal -= valToDecr;
+        }
+    }
+}
---
+++ A Good Idea/Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onClick ()
+    {
+        Debug.Log("clicked");
+    }
+}
